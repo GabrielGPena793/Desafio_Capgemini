3cbf1ac93a945a7ed6c140ffc80e3ed1404b9a91
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2022-02-15 20:54:17 -0300

    Challange 1

---
+++ desafio1/src/com/capgemini/Main.java
@@ -0,0 +1,26 @@
+package com.capgemini;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int n = 6;
+	    Map<Integer, String> ladder = new HashMap<>();
+
+        for (int i = 1; i <= n; i++) {
+            switch (i){
+                case 1 -> ladder.put(i,"     *");
+                case 2 -> ladder.put(i,"    **");
+                case 3 -> ladder.put(i,"   ***");
+                case 4 -> ladder.put(i,"  ****");
+                case 5 -> ladder.put(i," *****");
+                case 6 -> ladder.put(i,"******");
+            }
+            System.out.println(ladder.get(i));
+        }
+    }
+}
