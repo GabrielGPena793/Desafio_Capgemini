2303e3beba7d25d6d92fbdd422e4bc34063df9f5
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2022-02-16 01:38:40 -0300

    challange 2

---
+++ desafio2/src/com/capgemini/Constraints.java
@@ -0,0 +1,95 @@
+package com.capgemini;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Constraints {
+
+    List<String> errors = new ArrayList<>();
+
+    public List<String> getErrors() {
+        return errors;
+    }
+
+    public void addErro(String error){
+        errors.add(error);
+    }
+
+    //verificando tamanho minimo
+    public void minLength(String password){
+
+        if (password.length() < 6){
+            int characters = 6 - password.length();
+            errors.add("need " + characters + " more characters");
+
+        }
+    }
+
+    //verigicando digitos
+    public void checkDigit(String password){
+
+        boolean found = false;
+
+        for (int i = 0; i < password.length(); i++){
+            if (Character.isDigit(password.charAt(i))){
+               found = true;
+            }
+        }
+
+        if (!found){
+            errors.add("needs at least a number");
+        }
+
+    }
+
+    //verificnado letras maiusculas
+    public void upperLetter(String password){
+
+        boolean found = false;
+
+        for (char pass : password.toCharArray()){
+            if (pass >= 'A' && pass <= 'Z'){
+                found = true;
+            }
+        }
+
+        if (!found){
+            errors.add("need at least one capital letter");
+        }
+    }
+
+    //verificnado letras menusculas
+    public void lowerLetter(String password){
+
+        boolean found = false;
+
+        for (char pass : password.toCharArray()){
+            if (pass > 'a' && pass < 'z'){
+                found = true;
+            }
+        }
+
+        if (!found){
+            errors.add("need at least one lowercase letter");
+        }
+
+    }
+
+    //verificando caracter especial
+    public void specialFeature(String password){
+
+        boolean found = false;
+
+        for (int i = 0; i < password.length(); i++){
+            String letter = String.valueOf(password.charAt(i));
+
+            if (letter.matches("[!@#$%^&*()-+]")){
+                found = true;
+            }
+        }
+
+        if (!found){
+            errors.add("need at least special feature (!@#$%^&*()-+)");
+        }
+    }
+}
