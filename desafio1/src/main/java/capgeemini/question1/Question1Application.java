13b1fd874b3ee04e21618c4c847910dffda48402
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2022-02-17 20:04:21 -0300

    improving challange 1

---
+++ desafio1/src/main/java/capgeemini/question1/Question1Application.java
@@ -0,0 +1,25 @@
+package capgeemini.question1;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class Question1Application {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter the size of the drawning: ");
+		int number = scanner.nextInt();
+
+		String word = "*";
+
+		for (int i = number; i > 0; i--){
+			System.out.println(StringUtils.leftPad(word, number, ' '));
+			word = word + "*";
+		}
+	}
+
+}
